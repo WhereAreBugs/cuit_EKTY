0da4c7538ec7dc5fe8968445571db45bac2794a0
XXPXXPXXP <2572414306@qq.com>
2023-03-31 18:09:21 +0800

    修复了二维码参数的bug

--- main.py
+++ main.py
@@ -30,8 +30,10 @@ import time
 prefix = "http://ekty-cuit-edu-cn.webvpn.cuit.edu.cn:8118/#/pages/activity/studentQdqt?id="
 id = input("请输入活动ID: ")
 now_time = int(round(time.time() * 1000))
-now_time += 3600000  # 获取一小时之后的时间
-url = prefix + id + "&time=" + str(now_time)
-print("二维码链接: " + url)
+now_time += 14400000  # 获取两小时之后的时间
+url = prefix + id + "&timestamp=" + str(now_time)
+print("签到链接: " + url)
+print("有效期" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(now_time/1000)))
 img = qrcode.make(url)
+print("二维码已生成,请在当前目录下查看(qrcode.png)")
 img.save("qrcode.png")
